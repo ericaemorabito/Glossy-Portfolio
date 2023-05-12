a5dfc230f7d138727ee848b1375aed32ad1cb0cc
Erica <ericaemorabito@gmail.com>
2023-05-12 10:50:25 -0700

    responsive template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Header from './components/Header'
+import Home from './components/Home'
+import Template from './components/Template/template'
+
+import { Container } from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
       <Header expand={'false'} />
+      <Container className='main' fluid>
+        {/* <Home /> */}
+        <Template />
+      </Container>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../styles/home.css'
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
